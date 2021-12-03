c10b25b6a38108a5792c9d96f437ab6ec8d00ec4
Zhijie Xu <zhijie.xu@sjsu.edu>
2021-12-03 07:07:17 -0800

    completed part 1

---
+++ day3/part1.py
@@ -0,0 +1,70 @@
+# Use sample inputs
+# with open("sample.txt", "r") as file:
+# 	raw_binaries = file.readlines()
+
+with open("input.txt", "r") as file:
+	raw_binaries = file.readlines()
+
+# Handle raw_binaries
+binaries = map(lambda x: list(x.rstrip()), raw_binaries)
+binaries_rows = len(binaries)
+binaries_columns = len(binaries[0])
+
+
+def get_gamma_rate(array, columns, rows):
+	gamma_rate = []
+	for c in range(columns):
+		zero_count = 0
+		for r in range(rows):
+			if int(array[r][c]) == 0:
+				zero_count += 1
+		if zero_count > rows / 2:
+			gamma_rate.append(0)
+		else:
+			gamma_rate.append(1)
+	return gamma_rate
+
+
+gamma_binary_array = get_gamma_rate(binaries, binaries_columns, binaries_rows)
+
+
+def binary_to_decimal(binary):
+	bit_position = 0
+	decimal = 0
+	while binary > 0:
+		current_bit = binary % 10
+		decimal += current_bit * pow(2, bit_position)
+		binary = binary // 10
+		bit_position += 1
+	return decimal
+
+
+string_gamma_binary = [str(b) for b in gamma_binary_array]
+gamma_binary = int(''.join(string_gamma_binary))
+gamma_decimal = binary_to_decimal(gamma_binary)
+
+
+def get_epsilon_from_gamma(g_binary, g_decimal):
+	bits = len(str(g_binary))
+	epsilon = pow(2, bits) - 1 - g_decimal
+	return epsilon
+
+
+epsilon_decimal = get_epsilon_from_gamma(gamma_binary, gamma_decimal)
+
+
+print("What is the power consumption of the submarine?")
+print(gamma_decimal * epsilon_decimal)
+
+
+
+
+
+
+
+
+
+
+
+
+
